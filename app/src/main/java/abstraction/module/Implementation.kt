82384b98a5535684d8624b57d7065e22e1a30b91
Alireza Kamran <akamran@gmail.com>
2023-08-18 07:24:42 -0700

    save

--- app/src/main/java/abstraction/module/Implementation.kt
+++ app/src/main/java/abstraction/module/Implementation.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.*
 import kotlin.reflect.*
 import kotlinx.coroutines.*
 
-fun Fragment(interceptor: (Any, KFunction<*>, Array<*>, Runnable?) -> Pair<KFunction<*>?, Boolean?>?) =
-    OverlayFragment()
+fun Fragment(interceptor: (Any, KFunction<*>, Array<*>, Runnable?) -> Pair<KFunction<*>?, Boolean?>?): Pair<Fragment, Int?> =
+    Pair(OverlayFragment(), null)
 
 open class OverlayFragment : Fragment()
--- app/src/main/java/backgammon/module/BaseFragment.kt
+++ app/src/main/java/backgammon/module/BaseFragment.kt
@@ -16,6 +16,34 @@ abstract class BaseFragment : Fragment() {
     private val viewModel
         get() = activity?.asType<BaseActivity>()?.viewModel
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        launch @MainViewGroup {
+            Scheduler.EventBus.collect {
+                when (it?.transit) {
+                    ACTION_NAV_MAIN_UI ->
+                        viewModel?.apply {
+                            schedule {
+                                parentFragmentManager.commit {
+                                    transitFragment(view, savedInstanceState?.apply {
+                                        putShort(ACTION_KEY, ACTION_NAV_MAIN_UI)
+                                    }).let { overlay ->
+                                        setTransition(overlay.second ?: TRANSIT_FRAGMENT_OPEN)
+                                        replace(
+                                            this@BaseFragment.id,
+                                            overlay.first)
+                                    }
+                                }
+                            }
+                        }
+                    ACTION_MIGRATE_APP ->
+                        Scheduler.defer(::onViewCreated, Migration::class)
+                }
+            }
+        }
+        Log.i(UI_TAG, "Main fragment view is created.")
+    }
+
     override fun onAttach(context: Context) {
         super.onAttach(context)
         launch(Dispatchers.IO) @JobTreeRoot {
@@ -37,32 +65,6 @@ abstract class BaseFragment : Fragment() {
         }
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        launch @MainViewGroup {
-            Scheduler.EventBus.collect {
-                when (it?.transit) {
-                    ACTION_NAV_MAIN_UI ->
-                        viewModel?.apply {
-                            schedule {
-                                parentFragmentManager.commit {
-                                    setTransition(TRANSIT_FRAGMENT_OPEN)
-                                    replace(
-                                        this@BaseFragment.id,
-                                        transitFragment(view, savedInstanceState?.apply {
-                                            putShort(ACTION_KEY, ACTION_NAV_MAIN_UI)
-                                        }))
-                                }
-                            }
-                        }
-                    ACTION_MIGRATE_APP ->
-                        Scheduler.defer(::onViewCreated, Migration::class)
-                }
-            }
-        }
-        Log.i(UI_TAG, "Main fragment view is created.")
-    }
-
     override fun onResume() {
         if (State[1] === State.Finished) {
             with(Scheduler) {
@@ -76,7 +78,7 @@ abstract class BaseFragment : Fragment() {
         super.onResume()
     }
 
-    private var transitFragment = fun(_: View, bundle: Bundle?): Fragment {
+    private var transitFragment = fun(_: View, bundle: Bundle?): Pair<Fragment, Int?> {
         if (bundle?.getShort(ACTION_KEY, -1) == ACTION_NAV_MAIN_UI)
             abstraction.module.Fragment(::screenEventInterceptor).apply {
                 // ...
