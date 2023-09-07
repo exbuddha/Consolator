e8190f876a14fd03ef9a9696432ddcf403773f1b
Alireza Kamran <akamran@gmail.com>
2023-09-06 21:59:11 -0700

    save

--- consolator/src/main/java/net/consolator/BaseService.kt
+++ consolator/src/main/java/net/consolator/BaseService.kt
@@ -30,12 +30,12 @@ open class BaseService : Service(), BaseServiceScope, Provider {
                     sequencer {
                         if (logDb === null)
                             io(true) @Tag("log-db.build") {
-                                logDb = resetOnError { buildDatabase() }
+                                logDb = resetOnError(::buildDatabase)
                                 change(Context::stageLogDbCreated)
                             }
                         if (netDb === null)
                             io(true) @Tag("net-db.build") {
-                                netDb = resetOnError { buildDatabase() }
+                                netDb = resetOnError(::buildDatabase)
                                 // update net db records
                                 change(Context::stageNetDbInitialized)
                             }
