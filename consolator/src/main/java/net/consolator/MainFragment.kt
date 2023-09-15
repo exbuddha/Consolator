2b1a5b1779bb5d3496657c59adb925234a740286
Alireza Kamran <akamran@gmail.com>
2023-09-15 11:35:17 -0700

    save

--- consolator/src/main/java/net/consolator/MainFragment.kt
+++ consolator/src/main/java/net/consolator/MainFragment.kt
@@ -9,6 +9,14 @@ import net.consolator.BaseApplication.Companion.ABORT_NAV_MAIN_UI
 import net.consolator.BaseApplication.Companion.COMMIT_NAV_MAIN_UI
 
 class MainFragment : BaseFragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // show animation or progress bar
+        parentFragmentManager.commit {
+            show(this@MainFragment)
+        }
+    }
+
     override var overlay = fun(_: View, bundle: Bundle?): Pair<Fragment?, Int?> =
         when (bundle?.getShort(ACTION_KEY, -1)) {
             COMMIT_NAV_MAIN_UI ->
@@ -20,14 +28,6 @@ class MainFragment : BaseFragment() {
             else ->
                 throw BaseImplementationRestriction
         }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        // show animation or progress bar
-        parentFragmentManager.commit {
-            show(this@MainFragment)
-        }
-    }
 
     private inline fun <reified R> screenEventInterceptor(
         listener: Any,
