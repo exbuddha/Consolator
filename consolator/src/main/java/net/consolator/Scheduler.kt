5238b43489068769d98f362fcdda590e21fe705f
Alireza Kamran <akamran@gmail.com>
2024-01-03 17:32:18 -0800

    save

--- consolator/src/main/java/net/consolator/Scheduler.kt
+++ consolator/src/main/java/net/consolator/Scheduler.kt
@@ -125,7 +125,7 @@ object Scheduler : MutableLiveData<Step?>(), SchedulerScope, CoroutineContext, S
         private fun SequencerScope.form(stage: ContextStep): Step = { change(stage) }
         private fun SequencerScope.form(stage: ContextStep, vararg step: Step) = step.first() then form(stage)
 
-        private fun markTagsForReform(tag: String, stage: ContextStep?, form: Step, job: Job): Step =
+        private fun markTagsForReform(tag: String, stage: ContextStep?, form: Step, job: Job) =
             form.also { markTagsForCtxReform(tag, stage, form, job) }
 
         override fun commit(step: CoroutineStep) = clockAhead(step.markTagForSvcCommit()::invoke)
